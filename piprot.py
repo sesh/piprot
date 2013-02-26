2bc11b75382c0f5bb12d4d6ac5aca1d5c0db11a6
Brenton Cleeland <b.cleeland@gmail.com>
2013-02-27 09:46:31 +1100

    Removing the clint.colored stuff, home grown colouring thanks to SO

--- piprot.py
+++ piprot.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-from clint.textui import puts, colored
 from datetime import datetime
 from clint import args
-import json, time
+import json
+import time
 try:
     import cStringIO as StringIO
 except ImportError:
@@ -11,7 +11,32 @@ import urllib2
 import clint
 import sys
 
-def load_requirements(req_file, lint=False):
+
+class TextColours:
+    def __init__(self, enabled=False):
+        if enabled:
+            self.enable()
+        else:
+            self.disable()
+
+    def enable(self):
+        self.HEADER = '\033[95m'
+        self.OKBLUE = '\033[94m'
+        self.OKGREEN = '\033[92m'
+        self.WARNING = '\033[93m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
+
+def load_requirements(req_file, lint, colour=TextColours(False)):
     """
         Take a file and return a dict of (requirement, versions)
         based on the requirements files
@@ -28,11 +53,13 @@ def load_requirements(req_file, lint=False):
             except ValueError:
                 # what are you doing!
                 if lint:
-                    puts(colored.red('%s doesn\'t have a version number' % requirement))
+                    print '%s%s doesn\'t have a version number%s' % \
+                                (colour.FAIL, requirement, colour.ENDC)
 
     return req_dict
 
-def get_release_date(requirement, version=None):
+
+def get_release_date(requirement, version=None, colour=TextColours(False)):
     j = None
     try:
         if version:
@@ -41,20 +68,41 @@ def get_release_date(requirement, version=None):
             j = urllib2.urlopen('https://pypi.python.org/pypi/%s/json' % (requirement))
     except urllib2.HTTPError:
         if version:
-            puts(colored.red('%s (%s) isn\'t available on PyPi anymore!' % (requirement, version)))
+            print '%s%s (%s) isn\'t available on PyPi anymore!%s' % \
+                    (colour.FAIL, requirement, version, colour.ENDC)
         else:
-            puts(colored.red('%s isn\'t even on PyPi. Check that the project still exists!' % (requirement)))
+            print '%s%s isn\'t even on PyPi. Check that the project still exists!%s' % \
+                    (colour.FAIL, requirement, colour.ENDC)
         return None
+
     try:
         j = json.load(j)
         d = j['urls'][0]['upload_time']
         return datetime.fromtimestamp(time.mktime(time.strptime(d, '%Y-%m-%dT%H:%M:%S')))
     except IndexError:
-        puts(colored.red('%s (%s) didn\'t return a date property' % (requirement, version)))
+        print '%s%s (%s) didn\'t return a date property%s' % (colour.FAIL, requirement, version, colour.ENDC)
 
 if __name__ == '__main__':
+    # so, you need help do you?
+    if '--help' in args.all:
+        print 'piprot is a handle little tool that tells you just how rotten your ' + \
+                    'requirements are!'
+        print 'Turns out, they\'re pretty damn rotten!\n'
+        print 'Usage:\tpiprot <requirements-file> [OPTIONS]'
+        print '\tpip freeze | piprot [OPTIONS]'
+        print '\nAvailable options: '
+        print '  -c\t\tPretty colours'
+        print '  -l --lint\tEnable linting of your requirements file'
+        print '  -v\t\tVerbose output, you\'ll normally want this'
+        sys.exit()
+
     # use the first file as our requirements file
     req_file = None
+
+    # optionally, disable colour output
+    colour = TextColours(False)
+    if '-c' in args.all:
+        colour.enable()
 
     c = clint.piped_in()
     try:
@@ -67,7 +115,7 @@ if __name__ == '__main__':
             req_file = open(args.files[0])
 
     except IndexError:
-        puts(colored.red('You need to supply at least one filename'))
+        print '%sYou need to supply at least one filename%s' % (colour.FAIL, colour.ENDC)
         sys.exit()
 
     # are we being annoyingly verbose?
@@ -77,21 +125,15 @@ if __name__ == '__main__':
 
     # wanna add a little linting (this is in early stages)
     lint = False
-    if '--lint' in args.all:
+    if '--lint' in args.all or '-l' in args.all:
         lint = True
 
-    # optionally, disable colour output
-    colour = True
-    if '-c' in args.all:
-        colour = False
-    colour_proc = lambda c, s: colour and c(s) or s
-
     if req_file:
-        requirements = load_requirements(req_file, lint)
+        requirements = load_requirements(req_file, lint, colour)
         total_time_delta = 0
         for req, version in requirements.items():
-            latest_version = get_release_date(req)
-            specified_version = get_release_date(req, version)
+            latest_version = get_release_date(req, colour=colour)
+            specified_version = get_release_date(req, version, colour=colour)
 
             if latest_version and specified_version:
                 time_delta = (latest_version - specified_version).days
@@ -99,17 +141,16 @@ if __name__ == '__main__':
 
                 if verbose:
                     if time_delta > 0:
-                        puts(colour_proc(colored.yellow,
-                                         '%s (%s) is %s days out of date' % \
-                                         (req, version, time_delta)))
+                        print '%s%s (%s) is %s days out of date%s' % \
+                                (colour.FAIL, req, version, time_delta,
+                                        colour.ENDC)
                     else:
-                        puts(colour_proc(colored.green,
-                                         '%s (%s) is up to date' % \
-                                         (req, version)))
+                        print '%s%s (%s) is up to date%s' % \
+                                (colour.OKGREEN, req, version, colour.ENDC)
+
         if total_time_delta > 0:
-            puts(colour_proc(colored.red,
-                             "Your requirements are %s days out of date" % \
-                             total_time_delta))
+            print "%sYour requirements are %s days out of date%s" % \
+                            (colour.FAIL, total_time_delta, colour.ENDC)
         else:
-            puts(colour_proc(colored.green,
-                             "Looks like you've been keeping up to date, better go back to taming that beard!"))
+            print "%sLooks like you've been keeping up to date, better go back to taming that beard!%s" % \
+                                    (colour.OKGREEN, colour.ENDC)
