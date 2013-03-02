d602d6850107ae2f27a7d6f2b8960e418e67fc47
mark hellewell <mark.hellewell@icloud.com>
2013-03-02 13:08:08 +1100

    removed clint dependency, use stdlib argparse, YEAH!
    cleaner building of requirements dicts
    
    moved code to a main function
    
    started test suite
    
    added notes
    
    removed unused import and fixed syntax error
    
    fixed typo
    
    updated readme

--- piprot.py
+++ piprot.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-from clint import args
-import clint
+import argparse
 from datetime import datetime
-import json
 import os
 import requests
-import sys
 import time
 try:
     import cStringIO as StringIO
@@ -47,33 +44,31 @@ def get_pypi_url(requirement, version=None):
         return '{base}/{req}/json'.format(base=PYPI_BASE_URL, req=requirement)
 
 
-def load_requirements(req_file, lint, colour=TextColours(False)):
-    """
-        Take a file and return a dict of (requirement, versions)
-        based on the requirements files
+def parse_requirements_file(req_file, lint, colour=TextColours(False)):
+    """Take a file and return a dict of (requirement, versions) based
+    on the files requirements specs.
     """
     req_dict = {}
     requirements = req_file.readlines()
 
+    # TODO parse with regex
     for requirement in requirements:
         if '-r' in requirement[:3]:
             r, filename = requirement.replace('\n', '').strip().split(' ')
-            print 'Getting a little recursive, are we?'
-            req_dict = dict(req_dict.items() + load_requirements(open(os.path.join(os.path.dirname(req_file.name), filename)), lint, colour).items())
+            #print 'Getting a little recursive, are we?'
+            req_dict.update(parse_requirements_file(open(os.path.join(os.path.dirname(req_file.name), filename)), lint, colour=colour))
 
         requirement = requirement.replace('\n', '').strip().split(' ')[0]
         if requirement and requirement[0] not in ['#', '-'] and 'git' not in requirement:
             try:
                 requirement, version = requirement.split('==')
                 req_dict[requirement] = version
-                print requirement, version
+                #print requirement, version
             except ValueError:
                 # what are you doing!
                 if lint:
                     print '%s%s doesn\'t have a version number%s' % \
                                 (colour.FAIL, requirement, colour.ENDC)
-
-
     return req_dict
 
 
@@ -96,81 +91,60 @@ def get_release_date(requirement, version=None, colour=TextColours(False)):
     except IndexError:
         print '%s%s (%s) didn\'t return a date property%s' % (colour.FAIL, requirement, version, colour.ENDC)
 
-if __name__ == '__main__':
-    # so, you need help do you?
-    if '--help' in args.all:
-        print 'piprot is a handle little tool that tells you just how rotten your ' + \
-                    'requirements are!'
-        print 'Turns out, they\'re pretty damn rotten!\n'
-        print 'Usage:\tpiprot <requirements-file> [OPTIONS]'
-        print '\tpip freeze | piprot [OPTIONS]'
-        print '\nAvailable options: '
-        print '  -c\t\tPretty colours'
-        print '  -l --lint\tEnable linting of your requirements file'
-        print '  -v\t\tVerbose output, you\'ll normally want this'
-        sys.exit()
 
-    # use the first file as our requirements file
-    req_files = []
-
-    # optionally, enable colour output
-    colour = TextColours(False)
-    if '-c' in args.all:
-        colour.enable()
-
-    c = clint.piped_in()
-    try:
-        if c:
-            # some idiot piped something in
-            req_file = StringIO.StringIO(c)
-        else:
-            # the first file is all that matters. Yep.
-            # multi file support coming in 2.0
-            for f in args.files:
-                req_files.append(open(f))
-
-    except IndexError:
-        print '%sYou need to supply at least one filename%s' % (colour.FAIL, colour.ENDC)
-        sys.exit()
-
-    # are we being annoyingly verbose?
-    verbose = False
-    if '-v' in args.all:
-        verbose = True
-
-    # wanna add a little linting (this is in early stages)
-    lint = False
-    if '--lint' in args.all or '-l' in args.all:
-        lint = True
-
+def main(req_files=[], do_lint=False, do_colour=False, verbosity=0):
+    """Process a list of requirements to determine how out of date they are.
+    """
+    colour = TextColours(do_colour)
     requirements = {}
-    # basic multi file support
     for req_file in req_files:
-        requirements = dict(requirements.items() + load_requirements(req_file, lint, colour).items())
+        requirements.update(parse_requirements_file(req_file, do_lint, colour))
+        req_file.close()
 
-    print requirements
-    if len(requirements) > 0:
-        total_time_delta = 0
-        for req, version in requirements.items():
-            latest_version = get_release_date(req, colour=colour)
-            specified_version = get_release_date(req, version, colour=colour)
+    # close all files.
+    # remove duplicate requirements lines.
+    # divide requirements between a number of multiprocessing workers.
 
-            if latest_version and specified_version:
-                time_delta = (latest_version - specified_version).days
-                total_time_delta = total_time_delta + time_delta
+    total_time_delta = 0
+    for req, version in requirements.items():
+        latest_version = get_release_date(req, colour=colour)
+        specified_version = get_release_date(req, version, colour=colour)
 
-                if verbose:
-                    if time_delta > 0:
-                        print '%s%s (%s) is %s days out of date%s' % \
-                                (colour.FAIL, req, version, time_delta,
-                                        colour.ENDC)
-                    else:
-                        print '%s%s (%s) is up to date%s' % \
-                                (colour.OKGREEN, req, version, colour.ENDC)
+        if latest_version and specified_version:
+            time_delta = (latest_version - specified_version).days
+            total_time_delta = total_time_delta + time_delta
 
-        if total_time_delta > 0:
-            print "%sYour requirements are %s days out of date%s" % \
-                            (colour.FAIL, total_time_delta, colour.ENDC)
-        else:
-p            print "%sLooks like you've been keeping up to date, better go back to taming that beard!%s" % \
-                                    (colour.OKGREEN, colour.ENDC)
+            if verbosity:
+                if time_delta > 0:
+                    print '%s%s (%s) is %s days out of date%s' % \
+                      (colour.FAIL, req, version, time_delta,
+                       colour.ENDC)
+                else:
+                    print '%s%s (%s) is up to date%s' % \
+                        (colour.OKGREEN, req, version, colour.ENDC)
+
+    if total_time_delta > 0:
+        print "%sYour requirements are %s days out of date%s" % \
+          (colour.FAIL, total_time_delta, colour.ENDC)
+    else:
+        print "%sLooks like you've been keeping up to date, better go back to taming that beard!%s" % \
+          (colour.OKGREEN, colour.ENDC)
+
+
+if __name__ == '__main__':
+    cli_parser = argparse.ArgumentParser(
+        epilog="Here's hoping your requirements are nice and fresh!"
+        )
+    cli_parser.add_argument('-l', '--lint', action='store_true',
+                            help='lint the requirements')
+    cli_parser.add_argument('-c', '--colour', '--color', action='store_true',
+                            help='coloured output')
+    cli_parser.add_argument('-v', '--verbose', action='count',
+                            help='verbosity, can be supplied more than once')
+    cli_parser.add_argument('file', nargs='+', type=argparse.FileType(),
+                            help='requirements file(s), use `-` for stdin')
+    cli_args = cli_parser.parse_args()
+
+    # call the main function to kick off the real work
+    main(req_files=cli_args.file, do_lint=cli_args.lint,
+         do_colour=cli_args.colour, verbosity=cli_args.verbose)
