4dd552ec1b7eeacf5bc54121146855151757485e
Brenton Cleeland <b.cleeland@gmail.com>
2013-09-17 15:20:14 +1000

    Update requirements to use as test file. Add --verbatim option - my new favourite

--- piprot/piprot.py
+++ piprot/piprot.py
@@ -53,70 +53,48 @@ def get_pypi_url(requirement, version=None):
         return '{base}/{req}/json'.format(base=PYPI_BASE_URL, req=requirement)
 
 
-def parse_req_file(req_file, colour=TextColours(False)):
+def parse_req_file(req_file, colour=TextColours(False), verbatim=False):
     """Take a file and return a dict of (requirement, versions) based
     on the files requirements specs.
 
     TODO support pip's --index-url and --find-links requirements lines.
     TODO support Git, Subversion, Bazaar, Mercurial requirements lines.
     """
-    req_dict = {}
+    req_list = []
     requirements = req_file.readlines()
     for requirement in requirements:
-        if requirement.strip().startswith('#'):
-            continue
-
-        # if matching recursive requirement spec., call myself again
-        recursive = re.match('\s*-r\s+(?P<filename>\S+)', requirement)
-        if recursive:
-            try:
-                r_req_file = open(os.path.join(os.path.dirname(req_file.name),
-                                               recursive.group('filename')))
-                req_dict.update(parse_req_file(r_req_file, colour=colour))
-            except IOError:
-                print >> sys.stderr, '{} could not be opened'.format(
-                    recursive.group('filename')
-                )
-                continue
-
+        requirement_no_comments = requirement.split('#')[0].strip()
         # if matching requirement line (Thing==1.2.3), update dict, continue
         req_match = re.match('\s*(?P<package>\S+)==(?P<version>\S+)',
-                             requirement)
+                             requirement_no_comments)
         if req_match:
-            req_dict[req_match.group('package')] = req_match.group('version')
-            continue
-
-        # it matching VCS requirement spec., log to stderr, continue
-        vcs_match = re.match(
-            '\s*(-e){0,1}\s+(?P<vcsscheme>(svn|git|hg|bzr))\+',
-            requirement)
-
-        if vcs_match:
-            print >> sys.stderr, 'VCS requirements not yet supported'
-            continue
-        # if matches weird '>=1.2' format, log a warning, continue
-        # if no version spec, log a warning, continue
-        # if anything else, log a warning, continue?
-    return req_dict
+            req_list.append((req_match.group('package'), 
+                             req_match.group('version')))
+        else:
+            req_list.append((None, requirement))
+    return req_list
 
 
-def get_version_and_release_date(requirement, version=None, colour=TextColours(False)):
+def get_version_and_release_date(requirement, version=None, colour=TextColours(False), verbose=False):
     response = None
     try:
         response = requests.get(get_pypi_url(requirement, version)).json()
     except requests.HTTPError:
         if version:
-            print ('%s%s (%s) isn\'t available on PyPi anymore!%s' %
-                (colour.FAIL, requirement, version, colour.ENDC))
+            if verbose:
+                print ('%s%s (%s) isn\'t available on PyPi anymore!%s' %
+                      (colour.FAIL, requirement, version, colour.ENDC))
         else:
-            print ('%s%s isn\'t even on PyPi. Check that the project'
+            if verbose:
+                print ('%s%s isn\'t even on PyPi. Check that the project'
                 ' still exists!%s' %
                 (colour.FAIL, requirement, colour.ENDC))
         return None, None
     #TODO: Catch something more specific
     except:
-        print ('%sDecoding the JSON response for %s (%s) failed%s' %
-                (colour.FAIL, requirement, version, colour.ENDC))
+        if verbose:
+            print ('%sDecoding the JSON response for %s (%s) failed%s' %
+                  (colour.FAIL, requirement, version, colour.ENDC))
         return None, None
 
     try:
@@ -129,50 +107,62 @@ def get_version_and_release_date(requirement, version=None, colour=TextColours(F
             time.strptime(release_date, '%Y-%m-%dT%H:%M:%S')
         ))
     except IndexError:
-        print ('%s%s (%s) didn\'t return a date property%s' %
-            (colour.FAIL, requirement, version, colour.ENDC))
+        if verbose:
+            print ('%s%s (%s) didn\'t return a date property%s' %
+                (colour.FAIL, requirement, version, colour.ENDC))
         return None, None
 
 
-def main(req_files=[], do_colour=False, verbosity=0, latest=False):
+def main(req_files=[], do_colour=False, verbosity=0, latest=False, verbatim=False):
     """
         Process a list of requirements to determine how out of date they are.
     """
     colour = TextColours(do_colour)
-    requirements = {}
+
+    requirements = []
     for req_file in req_files:
-        requirements.update(parse_req_file(req_file, colour))
+        requirements.extend(parse_req_file(req_file, colour))
         req_file.close()
 
-    # close all files.
-    # remove duplicate requirements lines.
-    
     total_time_delta = 0
-    for req, version in requirements.items():
-        latest_version, latest_release_date = get_version_and_release_date(req, colour=colour)
-        specified_version, specified_release_date = get_version_and_release_date(req, version, colour=colour)
+    for req, version in requirements:
+        if verbatim and not req:
+            sys.stdout.write(version)
+        elif req:
+            latest_version, latest_release_date = get_version_and_release_date(req, colour=colour, verbose=(verbosity > 0))
+            specified_version, specified_release_date = get_version_and_release_date(req, version, colour=colour, verbose=(verbosity > 0))
 
-        if latest_release_date and specified_release_date:
-            time_delta = (latest_release_date - specified_release_date).days
-            total_time_delta = total_time_delta + time_delta
+            if latest_release_date and specified_release_date:
+                time_delta = (latest_release_date - specified_release_date).days
+                total_time_delta = total_time_delta + time_delta
 
-            if verbosity:
-                if time_delta > 0:
-                    print('%s%s (%s) is %s days out of date%s' %
-                          (colour.FAIL, req, version, time_delta, colour.ENDC), file=sys.stderr)
-                else:
-                    print('%s%s (%s) is up to date%s' %
-                        (colour.OKGREEN, req, version, colour.ENDC))
+                if verbosity:
+                    if time_delta > 0:
+                        print('%s%s (%s) is %s days out of date%s' %
+                              (colour.FAIL, req, version, time_delta, colour.ENDC), file=sys.stderr)
+                    else:
+                        print('%s%s (%s) is up to date%s' %
+                            (colour.OKGREEN, req, version, colour.ENDC))
 
-            if latest and latest_version != specified_version:
-                print('{}=={} # Updated from {}'.format(req, latest_version, specified_version))
+                if latest and latest_version != specified_version:
+                    print('{}=={} # Updated from {}'.format(req, latest_version, specified_version))
+                elif verbatim and latest_version != specified_version:
+                    print('{}=={} # Latest {}'.format(req, specified_version, latest_version))
+                elif verbatim:
+                    print('{}=={}'.format(req, specified_version))
+            else:
+                print('{}=={} # Error checking latest version'.format(req, version))
 
-    if total_time_delta > 0:
-        print("%sYour requirements are %s days out of date%s" %
-              (colour.FAIL, total_time_delta, colour.ENDC), file=sys.stderr)
+    if verbatim:
+        verbatim = "# Generated with piprot {}\n# ".format(VERSION)
     else:
-        print("%sLooks like you've been keeping up to date, time for a "
-              "delicious beverage!%s" % (colour.OKGREEN, colour.ENDC))
+        verbatim = ""
+    if total_time_delta > 0:
+        print("%s%sYour requirements are %s days out of date%s" %
+              (verbatim, colour.FAIL, total_time_delta, colour.ENDC), file=sys.stderr)
+    else:
+        print("%s%sLooks like you've been keeping up to date, time for a "
+              "delicious beverage!%s" % (verbatim, colour.OKGREEN, colour.ENDC))
 
 
 def piprot():
@@ -185,6 +175,8 @@ def piprot():
                             help='verbosity, can be supplied more than once')
     cli_parser.add_argument('-l', '--latest', action='store_true',
                             help='print the lastest available version for out of date requirements')
+    cli_parser.add_argument('-x', '--verbatim', action='store_true',
+                            help='output the full requirements file, with added comments with potential updates')
 
     # if there is a requirements.txt file, use it by default. Otherwise print
     # usage if there are no arguments.
@@ -200,9 +192,12 @@ def piprot():
 
     cli_args = cli_parser.parse_args()
 
+    if len(cli_args.file) > 1 and cli_args.verbatim:
+        sys.exit('--verbatim only allowed for single requirements files')
+
     # call the main function to kick off the real work
     main(req_files=cli_args.file, do_colour=cli_args.colour,
-         verbosity=cli_args.verbose, latest=cli_args.latest)
+         verbosity=cli_args.verbose, latest=cli_args.latest, verbatim=cli_args.verbatim)
 
 if __name__ == '__main__':
     piprot()
