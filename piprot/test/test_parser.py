5c5750db315308a922ce6c48b0a4a143b902c910
Brenton Cleeland <brenton@brntn.me>
2014-11-14 10:53:21 +1100

    Close your files, Brenton. :hankey:

--- piprot/test/test_parser.py
+++ piprot/test/test_parser.py
@@ -16,32 +16,38 @@ class TestRequirementsParser(unittest.TestCase):
         self.assertTrue(d[0][1] == '1.2.3')
 
     def test_requirements_file(self):
-        d = parse_req_file(open(os.path.join(os.path.dirname(__file__), 'files/_develop.txt')), verbatim=False)
-        self.assertTrue(d[0][0] == 'ipython')
-        self.assertTrue(d[0][1] == '1.1.0')
+        with open(os.path.join(os.path.dirname(__file__), 'files/_develop.txt')) as f:
+            d = parse_req_file(f, verbatim=False)
+            self.assertTrue(d[0][0] == 'ipython')
+            self.assertTrue(d[0][1] == '1.1.0')
 
     def test_recursive_requirements_file(self):
-        d = parse_req_file(open(os.path.join(os.path.dirname(__file__), 'files/test-requirements.txt')), verbatim=False)
-        reqs = [x[0] for x in d]
-        self.assertTrue('ipython' in reqs)
+        with open(os.path.join(os.path.dirname(__file__), 'files/test-requirements.txt')) as f:
+            d = parse_req_file(f, verbatim=False)
+            reqs = [x[0] for x in d]
+            self.assertTrue('ipython' in reqs)
 
     def test_requirements_length(self):
-        d = parse_req_file(open(os.path.join(os.path.dirname(__file__), 'files/_develop.txt')), verbatim=False)
-        self.assertEqual(len(d), 1)
+        with open(os.path.join(os.path.dirname(__file__), 'files/_develop.txt')) as f:
+            d = parse_req_file(f, verbatim=False)
+            self.assertEqual(len(d), 1)
 
     def test_recursive_requirements_length(self):
-        d = parse_req_file(open(os.path.join(os.path.dirname(__file__), 'files/test-requirements.txt')), verbatim=False)
-        self.assertEqual(len(d), 3)
+        with open(os.path.join(os.path.dirname(__file__), 'files/test-requirements.txt')) as f:
+            d = parse_req_file(f, verbatim=False)
+            self.assertEqual(len(d), 3)
 
     def test_requirements_file_verbatim(self):
-        d = parse_req_file(open(os.path.join(os.path.dirname(__file__), 'files/_develop.txt')), verbatim=True)
-        comments = [x[1] for x in d if not x[0]]
-        self.assertTrue('# Development Requirements\n' in comments)
+        with open(os.path.join(os.path.dirname(__file__), 'files/_develop.txt')) as f:
+            d = parse_req_file(f, verbatim=True)
+            comments = [x[1] for x in d if not x[0]]
+            self.assertTrue('# Development Requirements\n' in comments)
 
     def test_recursive_requirements_file_verbatim(self):
-        d = parse_req_file(open(os.path.join(os.path.dirname(__file__), 'files/_develop.txt')), verbatim=True)
-        comments = [x[1] for x in d if not x[0]]
-        self.assertTrue('# Development Requirements\n' in comments)
+        with open(os.path.join(os.path.dirname(__file__), 'files/_develop.txt')) as f:
+            d = parse_req_file(f, verbatim=True)
+            comments = [x[1] for x in d if not x[0]]
+            self.assertTrue('# Development Requirements\n' in comments)
 
 
 if __name__ == '__main__':
