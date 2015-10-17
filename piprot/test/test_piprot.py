17112c017648a1db37a7c191fc1a5c76246992b6
Christian Stefanescu <christian.stefanescu@native-instruments.de>
2015-10-17 16:50:19 +0200

    Cleaned up unused imports and variables

--- piprot/test/test_piprot.py
+++ piprot/test/test_piprot.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-import os
 import unittest
-from six import StringIO
 
 from piprot.piprot import main
+
 
 class TestRequirementsParser(unittest.TestCase):
     def setUp(self):
@@ -11,7 +10,7 @@ class TestRequirementsParser(unittest.TestCase):
 
     def test_requirement_exact(self):
         with self.assertRaises(SystemExit):
-            d = main([open('piprot/test/files/pytz_req.txt')])
+            main([open('piprot/test/files/pytz_req.txt')])
 
 
 if __name__ == '__main__':
